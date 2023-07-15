bfd6c68b8f77e744aa5c9a1c29377005b3a45055
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-15 06:25:02 +0300

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-    
+
   </BrowserRouter>,
 );
 
