72f4880d365f9abc3a480ad8d2f9c800acfcddd6
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-07-15 06:24:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
+    
   </BrowserRouter>,
 );
 
